e9e20153043b614323c6b333544deafb89b5b865
TheTimeTwerp <99409337+TheTimeTwerp@users.noreply.github.com>
2022-05-09 11:53:40 -0700

    Update math.js

--- math.js
+++ math.js
@@ -1,26 +1,20 @@
 function multiplyBy()
 {
-	num1 = document.getElementById("firstNumber").value;
-	num2 = document.getElementById("secondNumber").value;
-	document.getElementById("result").innerHTML = num1 * num2;
+  num1 = document.getElementById("firstNumber").value;
+  num2 = document.getElementById("secondNumber").value;
+  document.getElementById("result").innerHTML = num1 * num2;
+
 }
 
 function divideBy()
 {
-	num1 = document.getElementById("firstNumber").value;
-	num2 = document.getElementById("secondNumber").value;
-	document.getElementById("result").innerHTML = num1 / num2;
+  num1 = document.getElementById("firstNumber").value;
+  num2 = document.getElementById("secondNumber").value;
+  document.getElementById("result").innerHTML = num1 / num2;
+
 }
 
-function prime()
+function primeOrNot()
 {
-	num1 = document.getElementByID("prime").value;
-	result = false
-	for i in num1 do
-		if i>1
-			for j in range(2,i)
-				if(i % j == 0)
-					break
-				else
-					result = true
+
 }
